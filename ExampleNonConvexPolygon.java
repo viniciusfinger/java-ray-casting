573485e73117d69cfdd1f62dbe94e4b1bb20a48a
Finger <fingervinicius@icloud.com>
2024-01-12 12:59:46 -0300

    Adding more examples

---
+++ ExampleNonConvexPolygon.java
@@ -0,0 +1,20 @@
+import java.awt.geom.Point2D;
+import java.util.List;
+
+public class ExampleNonConvexPolygon {
+
+    public static void main(String[] args) {
+        
+        Point2D.Double testPoint = new Point2D.Double(0.4421937591102556, 101.88766462185495);
+
+        List<Point2D.Double> testPolygon = List.of(
+            new Point2D.Double(0.430, 101.881),
+            new Point2D.Double(0.451, 101.872),
+            new Point2D.Double(0.444, 101.885),
+            new Point2D.Double(0.448, 101.896)
+        );
+
+        boolean isInside = RayCasting.isPointInsidePolygon(testPoint, testPolygon);
+        System.out.println("Is the point inside the polygon? " + isInside);
+    }
+}
--- ExampleSquare.java
+++ ExampleSquare.java
@@ -1,17 +1,17 @@
 import java.awt.geom.Point2D;
 import java.util.List;
 
-public class Example {
+public class ExampleSquare {
 
     public static void main(String[] args) {
         
-        Point2D.Double testPoint = new Point2D.Double(0.439480686923888, 101.89079447127064);
+        Point2D.Double testPoint = new Point2D.Double(0.4421937591102556, 101.88766462185495);
 
         List<Point2D.Double> testPolygon = List.of(
-            new Point2D.Double(0.452, 101.873),
-            new Point2D.Double(0.439,101.867),
-            new Point2D.Double(0.429, 101.901),
-            new Point2D.Double(0.443, 101.905)
+            new Point2D.Double(0.430, 101.881),
+            new Point2D.Double(0.451, 101.872),
+            new Point2D.Double(0.444, 101.885),
+            new Point2D.Double(0.448, 101.896)
         );
 
         boolean isInside = RayCasting.isPointInsidePolygon(testPoint, testPolygon);
