ebf4a069e34b7f1850ae6ff2f7a2e782440e111c
Finger <fingervinicius@icloud.com>
2024-01-12 12:55:03 -0300

    Real sample

--- Example.java
+++ Example.java
@@ -5,14 +5,13 @@ public class Example {
 
     public static void main(String[] args) {
         
-        Point2D.Double testPoint = new Point2D.Double(-74.005974, 40.712776);
+        Point2D.Double testPoint = new Point2D.Double(0.439480686923888, 101.89079447127064);
 
         List<Point2D.Double> testPolygon = List.of(
-            new Point2D.Double(-74.006, 40.71),
-            new Point2D.Double(-74.006, 40.72),
-            new Point2D.Double(-74.002, 40.725),
-            new Point2D.Double(-73.997, 40.722),
-            new Point2D.Double(-73.994, 40.717)
+            new Point2D.Double(0.452, 101.873),
+            new Point2D.Double(0.439,101.867),
+            new Point2D.Double(0.429, 101.901),
+            new Point2D.Double(0.443, 101.905)
         );
 
         boolean isInside = RayCasting.isPointInsidePolygon(testPoint, testPolygon);
